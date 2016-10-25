d86a9dbb59d40495465dd6dc6f0189a2c9862c80
previtus <previtus@gmail.com>
2016-10-25 17:53:45 +0530

    Split, merge CU evaluation added

--- attempts/BobDylan.py
+++ attempts/BobDylan.py
@@ -1,12 +1,12 @@
 # BobDylan.py - our implementation of the Incremental Conceptual Clustering
 from Node import *
 from CU_measure import *
+import copy
 
 def BobDylan(N, O):
     # Add Object to the hierarchy represented by Node
 
     if N.isLeaf() and (N.objects == []):
-        print "case with empty tree"
         # first entry just put one child in!
         C = Node()
         N.appendChildObj(C)
@@ -18,7 +18,6 @@ def BobDylan(N, O):
         #N.updateCountMatrixFromChildren()
 
     elif N.isLeaf():
-        print "inserting in leaf"
         # insert here
         C1 = Node()
         C2 = Node()
@@ -38,7 +37,6 @@ def BobDylan(N, O):
         #print N.listAllObjects()
 
     else:
-        print "case two"
         # figure out what to do next
         N.addObjectsStats(O)
 
@@ -46,7 +44,9 @@ def BobDylan(N, O):
         CU_Child_Pairs = []
 
         #AllLists = range(1,5)
-        #print AllLists
+        #print "ALL-LISTS"
+        #DebugClusters(AllLists, 'BodyCover')
+
 
         for i in range(0, len(AllLists)):
             # for node N and its children C1 to Cn
@@ -59,10 +59,9 @@ def BobDylan(N, O):
             # get CU measure evaluation for clusters as [C1, ... Ci+O, ... Cn]
             withoutSel.append(selList)
 
-            # for debug
             clusters = withoutSel
             
-            DebugClusters(withoutSel, 'BodyCover')
+            #DebugClusters(withoutSel, 'BodyCover')
             cu = CU(clusters, attributes)
 
             CU_Child_Pairs.append([cu, i])
@@ -70,32 +69,78 @@ def BobDylan(N, O):
             selList.remove(O)
 
         CU_Child_Pairs = sorted(CU_Child_Pairs, key=lambda x: (-x[0]))
-        print CU_Child_Pairs
+        #print CU_Child_Pairs
 
         Cfirst_index = CU_Child_Pairs[0][1]
+        Cfirst = N.getChildrenById(Cfirst_index)
         Cfirst_score = CU_Child_Pairs[0][0]
         Csecond_index = CU_Child_Pairs[1][1]
 
         # singleton score
         SingletonList = AllLists + [[O]]
-        DebugClusters(SingletonList, 'BodyCover')
+        #DebugClusters(SingletonList, 'BodyCover')
         singletonScore = CU(SingletonList, attributes)
-        print singletonScore
+        #print singletonScore
 
+        # split score
+
+        #print "split the best?"
+        # split the best one into all its children!
+        splitScore = 0
+        if not Cfirst.isLeaf():
+            SplitList = copy.copy(AllLists)
+            del SplitList[Cfirst_index]
+            
+            for child_idx in Cfirst.children_indices:
+                child = Cfirst.getChildrenById(child_idx)
+                SplitList.append( child.listAllObjects() )
+
+            #DebugClusters(SplitList, 'BodyCover')
+            splitScore = CU(SplitList, attributes)
+        #print splitScore
+
+
+        # merge score
+        #print "merge two best?"
+        MergeList = copy.copy(AllLists)
+        a = Cfirst_index
+        b = Csecond_index
+        if b > a:
+            a = Csecond_index
+            b = Cfirst_index
+        del MergeList[a]
+        del MergeList[b]
+
+        MergeList.append( AllLists[a] + AllLists[b] + [O] )
+        
+        #DebugClusters(MergeList, 'BodyCover')
+        mergeScore = CU(MergeList, attributes)
+        #print mergeScore
+        
         # if C1 score is biggest?
-        if (Cfirst_score > singletonScore):
-            print "best is to add it to C1"
-            BobDylan(N.getChildrenById(Cfirst_index), O)
+        if (Cfirst_score > singletonScore) and (Cfirst_score > mergeScore) and (Cfirst_score > splitScore):
+            print "best"
+            BobDylan(Cfirst, O)
 
         # if singleton score is the best?
-        else:
-            print "best is to create new singleton"
+        elif (singletonScore > mergeScore) and (singletonScore > splitScore):
+            print "new"
             S = Node()
             S.addObjectsStats(O)
             S.addObject(O)
 
             N.appendChildObj(S)
-        
+
+        # is merge score is the best?
+        elif (mergeScore > splitScore):
+            print "merge"
+            # TODO
+            
+        # is split score is the best?
+        else:
+            print "split"
+            # TODO
+            
     return ""
 
 
@@ -107,7 +152,7 @@ O3 = {'BodyCover': 'hair', 'HeartChamber': 'four', 'BodyTemp': 'regulated', 'Fer
 O4 = {'BodyCover': 'feathers', 'HeartChamber': 'four', 'BodyTemp': 'regulated', 'Fertilization': 'internal'}
 O5 = {'BodyCover': 'cornified-skin', 'HeartChamber': 'imperfect-four', 'BodyTemp': 'unregulated', 'Fertilization': 'internal'}
 #Objects = [O1, O2]
-Objects = [O3, O4]
+Objects = [O3, O4, O5]
 
 Root = Node()
 #Root.reportTree()
@@ -131,7 +176,8 @@ Root.updateCountMatrixFromChildren()
 for Object in Objects:
     #print Object
     BobDylan( Root, Object )
-    Root.reportTree()
+
+Root.reportTree()
 
 
 #print "Final Tree:"
